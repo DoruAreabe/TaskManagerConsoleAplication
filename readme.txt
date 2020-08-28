9fb32a80bbaaf9fb86820d321ffca230ac6c4dd4
Mykyta Ponomarenko <mykpon3769@gmail.com>
2020-08-28 13:40:38 +0200

    TaskManager console application V2(final)

---
+++ readme.txt
@@ -0,0 +1,12 @@
+This is a simple task manager console aplication with data validation.
+You can:
+a) add Task  with title(title must be 28 signs or shorter), date(it must be in format xxx-xx-xx - where x - numbers, and mark important tasks;
+b) remove Task - just choose number of the task, and you can choose only numbers of added task.
+c) show already added Task with it numbers in a console
+d) exit aplication - when you exit application - task manager will write new data into data file. When you start aplication that data will be read to TaskManager.
+
+
+
+When you run program first time - task manager will check data file. If you dont have data file - he create it.
+If you have some trobles with input you get red message.
+
--- src/main/java/TaskManager.java
+++ src/main/java/TaskManager.java
@@ -1,3 +1,6 @@
+import org.apache.commons.lang3.Validate;
+import org.apache.commons.lang3.math.NumberUtils;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -17,26 +20,27 @@ public class TaskManager {
     private static void chooseAnOption(List<String> data) {
         Scanner scan = new Scanner(System.in);
         while (true) {
-            System.out.println("Please select the option:");
-            System.out.println("add | remove | list | exit");
+            System.out.println(ConsoleColors.BLUE + "Please select the option:");
+            System.out.println(ConsoleColors.RESET + "add | remove | list | exit");
             String temp = scan.nextLine();
             switch (temp.toLowerCase().trim()) {
                 case "add":
-                    data=add(data);
+                    data = add(data);
                     break;
                 case "remove":
                     data = remove(data);
                     break;
                 case "list":
                     for (String element : data) {
-                        System.out.println(data.indexOf(element)+" : "+element);
+                        System.out.println(data.indexOf(element) + " : " + element);
                     }
                     break;
                 case "exit":
                     exit(data);
                     return;
                 default:
-                    System.out.println("Not valid input, please tap again: ");
+                    System.out.println(ConsoleColors.RED + "Not valid input, please tap again: "
+                            + ConsoleColors.RESET);
                     continue;
             }
         }
@@ -46,27 +50,18 @@ public class TaskManager {
     private static List<String> add(List<String> data) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Please, type task description:");
-        String title = scan.nextLine();
+        String title = validateTitle();
         System.out.println("Please type task due date in format YYYY-MM-DD");
-        String date = scan.nextLine();
+        String date = validateDate();
         System.out.println("Is this task important? true/false");
-        String isImportant = "";
-        while (true) {
-            String temp = scan.nextLine();
-            if (!temp.equals("true") && !temp.equals("false")) {
-                System.out.println("Not valid format. Please tap true or false again");
-                continue;
-            }
-            isImportant = temp;
-            break;
-        }
-        String finalConcat =title+"  "+date+"  "+isImportant;
+        String isImportant = validateIsImp();
+        String finalConcat = title + "  " + date + "  " + isImportant;
         data.add(finalConcat);
         return data;
     }
 
     private static void exit(List<String> data) {
-        System.out.println("See you next time, bye!");
+        System.out.println(ConsoleColors.BLUE + "See you next time, bye!" + ConsoleColors.RESET);
         Path path = Paths.get("tasks.scv");
         try {
             Files.write(path, data);
@@ -81,9 +76,15 @@ public class TaskManager {
         while (true) {
             if (!scan.hasNextInt()) {
                 scan.next();
-                System.out.println("Input data not a number, please retype index again: ");
+                System.out.println(ConsoleColors.RED + "Input data not a number, please retype index again: "
+                        + ConsoleColors.RESET);
             }
             int index = scan.nextInt();
+            if (index < 0 || index > (data.size() - 1)) {
+                System.out.println(ConsoleColors.RED + "Sorry, that index is incorrect, please choose next one: "
+                        + ConsoleColors.RESET);
+                continue;
+            }
             data.remove(index);
             return data;
         }
@@ -97,17 +98,64 @@ public class TaskManager {
             try {
                 Files.createFile(path);
             } catch (IOException e) {
-                System.out.println("Failed fail creation");
+                System.out.println(ConsoleColors.RED+"Failed fail creation"+ConsoleColors.RESET);
             }
         }
         List<String> dataList = new ArrayList<>();
         try {
-            for (String listElenement : Files.readAllLines(path)) {
-                dataList.add(listElenement);
+            for (String listElement : Files.readAllLines(path)) {
+                dataList.add(listElement);
             }
         } catch (IOException e) {
-            System.out.println("You can't read data file");
+            System.out.println(ConsoleColors.RED+"Attempt read data file failed"+ConsoleColors.RESET);
         }
         return dataList;
+    }
+
+    private static String validateDate() {
+        Scanner scan = new Scanner(System.in);
+        while (true) {
+            String date = scan.nextLine();
+            String[] splitedData = date.split("-");
+            boolean isNumbers = true;
+            for (int i = 0; i < splitedData.length; i++) {
+                if (!NumberUtils.isParsable(splitedData[i])) isNumbers = false;
+            }
+            if (date.length() == 10 && splitedData.length == 3 && isNumbers) {
+                if (Integer.parseInt(splitedData[0]) > 2000 && Integer.parseInt(splitedData[0]) < 2100
+                        && Integer.parseInt(splitedData[1]) > 0 && Integer.parseInt(splitedData[1]) < 13
+                        && Integer.parseInt(splitedData[2]) > 0 && Integer.parseInt(splitedData[2]) < 32) {
+                    return date;
+                }
+            }
+            System.out.println(ConsoleColors.RED+"Input have invalid format, please retype date in format YYYY-MM-DD: "+ConsoleColors.RESET);
+        }
+    }
+
+    private static String validateTitle() {
+        Scanner scan = new Scanner(System.in);
+        while (true) {
+            String title = scan.nextLine();
+            if (title.length() >= 29) {
+                System.out.println(ConsoleColors.RED+"Type shorter task description, please: "+ConsoleColors.RESET);
+                continue;
+            }
+            while (title.length() != 30) {
+                title += " ";
+            }
+            return title;
+        }
+    }
+
+    private static String validateIsImp() {
+        Scanner scan = new Scanner(System.in);
+        while (true) {
+            String isImportant = scan.nextLine();
+            if (!isImportant.equals("true") && !isImportant.equals("false")) {
+                System.out.println(ConsoleColors.RED+"Not valid format. Please tap true or false again"+ConsoleColors.RESET);
+                continue;
+            }
+            return isImportant;
+        }
     }
 }
